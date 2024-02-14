3089377f4ff41f0d1fcfc75e4337ad6e6924828c
Ihor Nikolskyi <ihor.nikolskyi@shelf.io>
2024-02-14 19:36:19 +0200

    improve birthday list and fix formatting

--- task_1/greet_collegues.py
+++ task_1/greet_collegues.py
@@ -1,6 +1,6 @@
 import random
-from datetime import datetime, timedelta, date
 from collections import defaultdict
+from datetime import date, datetime, timedelta
 
 
 def create_random_users() -> list[dict]:
@@ -8,23 +8,53 @@ def create_random_users() -> list[dict]:
     Create random users all-over the year
     """
     names = [
-        'John', 'Sarah', 'Robert', 'Emma', 'Michael', 'Sophia', 
-        'James', 'Emily', 'David', 'Olivia', 'Richard', 'Isabella', 
-        'Charles', 'Ava', 'Joseph', 'Mia', 'Thomas', 'Charlotte'
+        "John",
+        "Sarah",
+        "Robert",
+        "Emma",
+        "Michael",
+        "Sophia",
+        "James",
+        "Emily",
+        "David",
+        "Olivia",
+        "Richard",
+        "Isabella",
+        "Charles",
+        "Ava",
+        "Joseph",
+        "Mia",
+        "Thomas",
+        "Charlotte",
     ]
     surnames = [
-        'Smith', 'Johnson', 'Brown', 'Taylor', 'Miller', 'Davis', 
-        'Clark', 'Rodriguez', 'Lewis', 'Lee', 'Walker', 'White', 
-        'Jackson', 'Harris', 'Martin', 'Thompson', 'Garcia', 'Martinez'
+        "Smith",
+        "Johnson",
+        "Brown",
+        "Taylor",
+        "Miller",
+        "Davis",
+        "Clark",
+        "Rodriguez",
+        "Lewis",
+        "Lee",
+        "Walker",
+        "White",
+        "Jackson",
+        "Harris",
+        "Martin",
+        "Thompson",
+        "Garcia",
+        "Martinez",
     ]
     random_users = []
     for day_of_year in range(1, 360):
         # get the next date
         date_of_year = datetime(datetime.today().year, 1, 1) + timedelta(day_of_year - 1)
-        
-        # decide randomly how many birthdays will be in a day from 0 to 3
-        num_birthdays = random.randint(0, 3)
-        
+
+        # decide randomly how many birthdays will be in a day from 0 to 2
+        num_birthdays = random.randint(0, 2)
+
         # generate users with random names per day, if any
         for _ in range(num_birthdays):
             chosen_name = f"{random.choice(names)} {random.choice(surnames)}"
@@ -40,8 +70,8 @@ def get_birthdays_per_week(collegues: list[dict], refference_date: date = dateti
     If a user's birthday falls on a weekend, it is considered as falling on the next Monday.
     """
 
-    weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-    next_week_birthday_users = defaultdict(list)
+    weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+    next_week_birthday_colleagues = defaultdict(list)
 
     for colleague in collegues:
         name = colleague["name"]
@@ -55,17 +85,19 @@ def get_birthdays_per_week(collegues: list[dict], refference_date: date = dateti
 
         if delta_days >= 0 and delta_days < 7:
             if birthday_this_year.weekday() >= 5:  # if it's a weekend
-                weekday = 'Monday'
+                weekday = "Monday"
             else:
                 weekday = weekdays[birthday_this_year.weekday()]
-            next_week_birthday_users[weekday].append(name)
+            next_week_birthday_colleagues[weekday].append(f"{name} ({birthday_this_year.strftime('%Y-%m-%d')})")
 
-    for day, names in next_week_birthday_users.items():
+    print(
+        f"List of colleagues to greet for the next week, as of {refference_date.strftime('%Y-%m-%d')}, {weekdays[refference_date.weekday()]}:"
+    )
+    print("---")
+    for day, names in next_week_birthday_colleagues.items():
         print(f'{day}: {", ".join(names)}')
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     users = create_random_users()
-    print(f"Total number of users: {len(users)}")
-    print(f"Each user follows next structure {users[0]}")
     get_birthdays_per_week(users)
