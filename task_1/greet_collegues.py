3d009f5b0258a215a73bc6f22611e3dfb626bd59
Ihor Nikolskyi <ihor.nikolskyi@shelf.io>
2024-02-14 21:20:32 +0200

    split get_birthdays_per_week for better readability

--- task_1/greet_collegues.py
+++ task_1/greet_collegues.py
@@ -1,9 +1,52 @@
 from collections import defaultdict
 from datetime import date, datetime
+
 from utils import create_random_users
 
+WEEKDAYS = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
 
-def get_birthdays_per_week(collegues: list[dict], relative_date: date = datetime.today().date()):
+
+def get_birthday_this_year(birthday: date, relative_date: date) -> date:
+    """
+    Returns this year's birthday based on a given reference date.
+
+    Args:
+        birthday (date): The birthday date.
+        relative_date (date): The reference date.
+
+    Returns:
+        date: This year's birthday based on the reference date.
+    """
+
+    birthday_this_year = birthday.replace(year=relative_date.year)
+    if birthday_this_year < relative_date:
+        birthday_this_year = birthday_this_year.replace(year=relative_date.year + 1)
+
+    return birthday_this_year
+
+
+def get_greeting_day(birthday_this_year: date, relative_date: date) -> str:
+    """
+    Returns the weekday to greet the colleague.
+
+    Args:
+        birthday_this_year (date): The colleague's birthday this year.
+        relative_date (date): The reference date.
+
+    Returns:
+        str: The weekday to greet the colleague.
+    """
+
+    # if birthday is a weekend and relative_date is Monday, so greeting goes to next Monday
+    if (birthday_this_year.weekday() >= 5) & (relative_date.weekday() == 0):
+        return "Next Monday"
+    elif birthday_this_year.weekday() >= 5:
+        return "Monday"
+    else:
+        return WEEKDAYS[birthday_this_year.weekday()]
+
+
+def get_birthdays_per_week(collegues: list[dict], relative_date: date = datetime.today().date()) -> str:
     """
     Determine which colleagues have birthdays in the upcoming week.
 
@@ -13,51 +56,41 @@ def get_birthdays_per_week(collegues: list[dict], relative_date: date = datetime
 
         relative_date (date, optional): The date from which to calculate one week forward. Default is today's date.
 
-        weekdays (list[str], optional): List of weekday names. Defaults to predefined WEEKDAYS list.
-
     Returns:
         str: A formatted string containing the names of colleagues that have birthdays
             in the upcoming week, sorted by the day of the week.
     """
-
     weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
     next_week_birthday_colleagues = defaultdict(list)
 
     for colleague in collegues:
-        # prepare colleague birthday information
         name = colleague["name"]
         birthday = colleague["birthday"]
-        birthday_this_year = birthday.replace(year=relative_date.year)
+        birthday_this_year = get_birthday_this_year(birthday, relative_date)
 
-        if birthday_this_year < relative_date:
-            birthday_this_year = birthday_this_year.replace(year=relative_date.year + 1)
-
-        # get the colleagues with birthdays in the next 7 days, including relative_date
         delta_days = (birthday_this_year - relative_date).days
-        # TODO: handle next monday situation
-        if delta_days >= 0 and delta_days < 7:
-            # if BD's on a weekend and relative_date is Monday, so greeting goes to next Monday
-            if (birthday_this_year.weekday() >= 5) & (relative_date.weekday() == 0):
-                weekday = "Next Monday"
-            # if BD's on a weekend and goes to Monday
-            elif birthday_this_year.weekday() >= 5:
-                weekday = 'Monday'
-            else:
-                weekday = weekdays[birthday_this_year.weekday()]
-            next_week_birthday_colleagues[weekday].append(f"{name} ({birthday_this_year.strftime('%Y-%m-%d')})")
-    # print colleagues with birthdays in the next 7 days, including relative_date
+        if delta_days < 7:
+            weekday_to_greet = get_greeting_day(birthday_this_year, relative_date)
+            next_week_birthday_colleagues[weekday_to_greet].append(
+                f"{name} ({birthday_this_year.strftime('%Y-%m-%d')})"
+            )
+
     relative_date_str = f"{relative_date.strftime('%Y-%m-%d')}, {weekdays[relative_date.weekday()]}"
-    print(
-        f"---\nColleagues to greet for the next week, as of {relative_date_str}:\n---"
-    )
-    for day, names in next_week_birthday_colleagues.items():
-        print(f'{day}: {", ".join(names)}')
+
+    greeting_string = f"---\nColleagues to greet for the next week, as of {relative_date_str}:\n---"
+    if next_week_birthday_colleagues:
+        for day, names in next_week_birthday_colleagues.items():
+            greeting_string += f"\n{day}: {', '.join(names)}"
+    else:
+        greeting_string += "\nNo birthdays this week :("
+
+    return greeting_string
 
 
 if __name__ == "__main__":
     users = create_random_users()
     # example of checking birthdays from Monday
     monday = date(2024, 2, 12)
-    get_birthdays_per_week(users, monday)
+    print(get_birthdays_per_week(users, monday))
     # example of checking birthdays from today
-    get_birthdays_per_week(users)
+    print(get_birthdays_per_week(users))
